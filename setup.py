a827b32cb1dc209625ef7dc238185e0c069139af
agitator <hpeter@agitator.com>
2011-08-31 14:01:54 +0000

    version push

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.0'
+version = '1.0.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
