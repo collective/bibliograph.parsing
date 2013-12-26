6144200f5a4b8a0bb43ef1cbf357bcb76e336650
cewing <cris@crisewing.com>
2013-12-26 11:16:31 -0700

    remove overly specific version specifier

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.2-jazdev'
+version = '1.0.2-dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
